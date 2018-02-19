93fd31fa1281828f6f67fa7b0d569ff392e4ee85
Anushka Bohara <anushka.bohara@gmail.com>
2018-02-19 22:02:32 +0530

    new file:   Cookit/.gitignore 	new file:   Cookit/.idea/.name 	new file:   Cookit/.idea/compiler.xml 	new file:   Cookit/.idea/copyright/profiles_settings.xml 	new file:   Cookit/.idea/encodings.xml 	new file:   Cookit/.idea/gradle.xml 	new file:   Cookit/.idea/misc.xml 	new file:   Cookit/.idea/modules.xml 	new file:   Cookit/.idea/runConfigurations.xml 	new file:   Cookit/app/.gitignore 	new file:   Cookit/app/build.gradle 	new file:   Cookit/app/proguard-rules.pro 	new file:   Cookit/app/src/androidTest/java/com/example/anushka/cookit/ApplicationTest.java 	new file:   Cookit/app/src/main/AndroidManifest.xml 	new file:   Cookit/app/src/main/java/com/example/anushka/cookit/Quantity.java ew file:   Cookit/app/src/main/java/com/example/anushka/cookit/request.java 	new file:   Cookit/app/src/main/res/layout/activity_quantity.xml 	new file:   Cookit/app/src/main/res/layout/activity_request.xml 	new file:   Cookit/app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   Cookit/app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   Cookit/app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   Cookit/app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   Cookit/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   Cookit/app/src/main/res/values-w820dp/dimens.xml 	new file:   Cookit/app/src/main/res/values/colors.xml 	new file:   Cookit/app/src/main/res/values/dimens.xml 	new file:   Cookit/app/src/main/res/values/strings.xml 	new file:   Cookit/app/src/main/res/values/styles.xml 	new file:   Cookit/app/src/test/java/com/example/anushka/cookit/ExampleUnitTest.java 	new file:   Cookit/build.gradle 	new file:   Cookit/gradle.properties 	new file:   Cookit/gradle/wrapper/gradle-wrapper.jar 	new file:   Cookit/gradle/wrapper/gradle-wrapper.properties 	new file:   Cookit/gradlew 	new file:   Cookit/gradlew.bat 	new file:   Cookit/settings.gradle

---
+++ Cookit/app/src/main/java/com/example/anushka/cookit/request.java
@@ -0,0 +1,23 @@
+package com.example.anushka.cookit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class request extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Quantity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Cooking request sent for rice for " + message + " people.");
+    }
+}
