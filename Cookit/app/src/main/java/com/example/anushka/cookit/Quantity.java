59df926a2f23026a46f1753d0f2bc0b7c3027b56
Anushka Bohara <anushka.bohara@gmail.com>
2018-02-21 00:41:05 +0530

    Initial commit

--- Cookit/app/src/main/java/com/example/anushka/cookit/Quantity.java
+++ Cookit/app/src/main/java/com/example/anushka/cookit/Quantity.java
@@ -5,10 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Quantity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +24,41 @@ public class Quantity extends AppCompatActivity {
         Intent intent = new Intent(this, request.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        int num = Integer.parseInt(message);
+
+        if ((num > 0) || (num <= 10)){
+            intent.putExtra(EXTRA_MESSAGE, message);
+            Long tsLong = System.currentTimeMillis()/1000;
+            String ts = tsLong.toString();
+            mDatabase = FirebaseDatabase.getInstance().getReference();
+            mDatabase.child("rice").child(ts).child("Quantity").setValue(num);
+
+            startActivity(intent);
+        }
+        else{
+            TextView text2 = (TextView) findViewById(R.id.textView2);
+            text2.setText("Qunatity should be from 1 to 10");
+        }
+
+
     }
 
+    public void incVal(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String value = editText.getText().toString();
+        int qty = Integer.parseInt(value);
+        if (qty < 10) {
+            qty = qty + 1;
+        }
+        editText.setText(""+qty+"");
+    }
+    public void decVal(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String value = editText.getText().toString();
+        int qty = Integer.parseInt(value);
+        if (qty > 1) {
+            qty = qty - 1;
+        }
+        editText.setText(""+qty+"");
+    }
 }
